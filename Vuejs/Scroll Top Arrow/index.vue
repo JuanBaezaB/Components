dabf32cd0509a73e2d18eff6657293f3f7c0be2b
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-08-02 17:45:40 -0400

    navbar

---
+++ Vuejs/Scroll Top Arrow/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <p>A super long component</p>
+        <ScrollTopArrow></ScrollTopArrow>
+    </div>
+</template>
+<script>
+    import ScrollTopArrow from '@/components/shared/blog/ScrollTopArrow'
+    export default {
+    components: {
+        ScrollTopArrow
+    }
+    }
+</script>
